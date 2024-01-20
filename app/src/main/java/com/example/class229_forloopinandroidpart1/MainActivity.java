961f2eeb224272b5ccdbce1f2850c5663fc6468e
This pc <yousuf01865452954@gmail.com>
2024-01-20 23:38:37 +0600

    class 229 for loop 1000 button

--- app/src/main/java/com/example/class229_forloopinandroidpart1/MainActivity.java
+++ app/src/main/java/com/example/class229_forloopinandroidpart1/MainActivity.java
@@ -4,29 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-Button button;
+LinearLayout linearlayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button=findViewById(R.id.button);
-
-        button.setText(" ");
-
-        for(int x=1;x<=1000;x++){
+        linearlayout=findViewById(R.id.linearlayout);
 
 
 
-            if(x%5==0 &&x%11==0)  button.append(" "+x);
+        for (int x=1;x<1000;x++){
 
+            Button button =new Button(MainActivity.this);
 
+            button.setText("Button"+x);
+
+            linearlayout.addView(button);
 
         }
+
+
+
 
 
     }
